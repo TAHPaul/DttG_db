1c913f70902edb6080b31a4ff1a7e500024c4e00
TAH_Paul <101122668+TAHPaul@users.noreply.github.com>
2024-05-07 14:04:08 +0100

    push to vercel
    https://www.youtube.com/watch?v=I5x8lAVQ8QQ

--- dttg_new/settings.py
+++ dttg_new/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #makes a folder 'media' in the basedir 
 MEDIA_URL = '/media/'
 
